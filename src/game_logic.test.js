2541abf2d8f4c48745c5cd1677cb8e746cafdbf6
marink <marink@uni-bremen.de>
2022-11-11 22:44:27 +0100

    refactored tests

---
+++ src/game_logic.test.js
@@ -0,0 +1,18 @@
+test("check every combination for SSP", () => {
+    expect(calculateWinner(GameSymbol.Stein, GameSymbol.Stein)).toBe(GameWinner.Nobody);
+    expect(calculateWinner(GameSymbol.Stein, GameSymbol.Papier)).toBe(GameWinner.Computer);
+    expect(calculateWinner(GameSymbol.Stein, GameSymbol.Schere)).toBe(GameWinner.Player);
+    expect(calculateWinner(GameSymbol.Papier, GameSymbol.Stein)).toBe(GameWinner.Player);
+    expect(calculateWinner(GameSymbol.Papier, GameSymbol.Papier)).toBe(GameWinner.Nobody);
+    expect(calculateWinner(GameSymbol.Papier, GameSymbol.Schere)).toBe(GameWinner.Computer);
+    expect(calculateWinner(GameSymbol.Schere, GameSymbol.Stein)).toBe(GameWinner.Computer);
+    expect(calculateWinner(GameSymbol.Schere, GameSymbol.Papier)).toBe(GameWinner.Player);
+    expect(calculateWinner(GameSymbol.Schere, GameSymbol.Schere)).toBe(GameWinner.Nobody);
+})
+
+//TODO test SSPB
+/** 
+test("check every combination for SSPB"), () => {
+
+}
+*/
